bb818baac3004ca24eabe937b51c2e57dd01a7b2
ingridrochaa <ingrid.vinticinco@escola.pr.gov.br>
2026-03-26 09:34:48 -0300

    prova

---
+++ ex1.js
@@ -0,0 +1,9 @@
+//crie duas variaveis e mostrar; soma, Subtração e Multiplicação
+
+let n1 = 10 
+let n2 = 5; 
+let soma = n1+n2;
+let Subtração = n1-n2;
+let Multiplicação = n1*n2;
+
+console.log(`A soma de ${n1} mais ${n2} é igual  ${soma}, o resultado da Subtração é ${Subtração} e da Multiplicação é ${Multiplicação}`);
